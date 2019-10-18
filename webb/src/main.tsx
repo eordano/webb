800a78699232cbdf9046c12204c4514ac6a607bb
Esteban Ordano <esteban@decentraland.org>
2019-10-18 09:37:23 -0300

    wip

--- webb/src/main.tsx
+++ webb/src/main.tsx
@@ -1,14 +1,11 @@
 import * as React from 'react'
 import * as ReactDOM from 'react-dom'
-import { createStore } from 'redux'
-import { Provider } from 'react-redux'
-
-const store = createStore(() => ({}))
+import App from './App'
 
 ReactDOM.render(
   <>
-    <Provider store={store}>
-    <h1>Hello world!</h1></Provider>
+    <h1>Hello world!</h1>
+    <App />
   </>,
   document.getElementById('root')
 )
