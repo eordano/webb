66f3f9083b5835bd3ade77bfda85f144f570d357
Esteban Ordano <esteban@decentraland.org>
2019-10-23 00:16:43 -0300

    chore: add logs to content server

--- descartes/bin/cachedServer.ts
+++ descartes/bin/cachedServer.ts
@@ -20,6 +20,7 @@ export function createServer(descartes: Descartes, port: number = 1337) {
       return res.status(400).json(JSON.stringify({ error: 'Bad request, x1, y1, x2, y2 are required' }))
     }
     const { x1, x2, y1, y2 } = req.query
+    console.log(`Queried: ${JSON.stringify(req.query)}`)
     const mapToScene = await descartes.getSceneIdForCoordinates(everythingInside(x1, x2, y1, y2))
     res.send({
       data: Object.keys(mapToScene).map(_ => ({ parcel_id: _, scene_cid: mapToScene[_], root_cid: mapToScene[_] }))
@@ -31,21 +32,23 @@ export function createServer(descartes: Descartes, port: number = 1337) {
       return res.status(400).json(JSON.stringify({ error: 'Bad request, cids array argument is required' }))
     }
     const { cids } = req.query
-    const sceneMap = await descartes.getMappingForSceneId(cids)
+    console.log(`Mapping: ${cids}`)
+    const sceneMap = await Promise.all(cids.split(',').map(async (_: string) => [_, await descartes.getMappingForSceneId(cids)]))
     res.send({
-      data: Object.keys(sceneMap).map(_ => ({
-        root_cid: _,
-        scene_cid: _,
-        publisher: _,
+      data: sceneMap.map(([sceneId, currentMap]) => ({
+        root_cid: sceneId,
+        scene_cid: sceneId,
+        publisher: sceneId,
         content: {
-          contents: Object.keys(sceneMap[_]).map($ => ({ file: $, hash: sceneMap[_][$] }))
+          contents: Object.keys(currentMap).map(file => ({ file, hash: currentMap[file] }))
         }
       }))
     })
   })
 
   app.get('/contents/:cid', async (req, res) => {
-      res.send(await descartes.getContent(req.param['cid']))
+    console.log(`Fetch content: ${req.params['cid']}`)
+    res.end(await descartes.getContent(req.params['cid']))
   })
 
   return app.listen(port)
