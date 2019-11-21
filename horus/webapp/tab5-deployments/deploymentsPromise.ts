8adb6ca9c9c69b0c344295b9bc1952a762e9de4b
Pablo de Haro <pablitar@gmail.com>
2019-11-21 12:14:03 -0300

    Changed the implementation to use the "files" metabase field instead of parcel_info

--- horus/webapp/tab5-deployments/deploymentsPromise.ts
+++ horus/webapp/tab5-deployments/deploymentsPromise.ts
@@ -1,39 +1,42 @@
-import { HORUS_CONFIG } from "../horus-config/config";
+import { HORUS_CONFIG } from '../horus-config/config'
 
 async function fetchJson(contentServerPath: string, options?: RequestInit) {
-    const response = await fetch(`${HORUS_CONFIG.CONTENT_SERVER_URL}/${contentServerPath}`)
-    if(response.status >= 400) throw new Error(`Unexpected response from content server: ${response.status}`);
+  const response = await fetch(`${HORUS_CONFIG.CONTENT_SERVER_URL}/${contentServerPath}`)
+  if (response.status >= 400) throw new Error(`Unexpected response from content server: ${response.status}`)
 
-    return await(response.json())
+  return await response.json()
 }
 
-async function sceneName(cid: string, infos: any[]) {
-    const sceneInfo = infos.find(it => it.scene_cid === cid)
-    
-    if(!sceneInfo || !sceneInfo.content || !sceneInfo.content.contents) return {cid, name: ''}
+async function sceneName(deployment) {
+  try {
+    const jsonContentFile = JSON.parse(deployment.files).find(it => {
+      const path = it.paths ? it.paths[0] : null
+      return path === 'scene.json'
+    })
 
-    const jsonContent = sceneInfo.content.contents.find(it => it.file == "scene.json")
+    if (!jsonContentFile) return { cid: deployment.cid, name: '' }
 
-    const sceneData = await fetchJson("contents/" + jsonContent.hash)
-    
-    return sceneData ? { cid, name: sceneData.display.title } : {cid, name: ''}
+    const sceneData = await fetchJson('contents/' + jsonContentFile.cid)
+
+    return sceneData ? { cid: deployment.cid, name: sceneData.display.title } : { cid: deployment.cid, name: '' }
+  } catch (e) {
+    return { cid: deployment.cid, name: '' };
+  }
 }
 
-async function sceneNames(cids: Array<string>) {
-    const infos = await fetchJson(`parcel_info?cids=${cids.join(',')}`)
+async function sceneNames(deployments: Array<any>) {
+  const nameList = await Promise.all(deployments.map(deplyment => sceneName(deplyment)))
 
-    const nameList = await Promise.all(cids.map(cid => sceneName(cid, infos.data)))
-
-    return nameList.reduce((dict, cidNamePair) => Object.assign(dict, {[cidNamePair.cid]: cidNamePair.name}), {});
+  return nameList.reduce((dict, cidNamePair) => Object.assign(dict, { [cidNamePair.cid]: cidNamePair.name }), {})
 }
 
 export async function deployments() {
-    const deployments = await fetchJson(`deployments`)
-    const names = await sceneNames(deployments.map(it => it.cid))
+  const deployments = await fetchJson(`deployments`)
+  const names = await sceneNames(deployments)
 
-    deployments.forEach(deployment => {
-        deployment.name = names[deployment.cid]
-    });
+  deployments.forEach(deployment => {
+    deployment.name = names[deployment.cid]
+  })
 
-    return deployments;
+  return deployments
 }
