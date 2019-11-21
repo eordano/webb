b87b7279c5ef66cad9180f1c8fce849a6959a252
Pablo de Haro <pablitar@gmail.com>
2019-11-21 12:59:23 -0300

    Reverted a couple of changes that now are unnecessary

--- horus/webapp/kernel/scenes.ts
+++
@@ -1,9 +0,0 @@
-import { ParcelInfoResponse } from "dcl/utils";
-import { fetchManifestForContent } from "dcl/kernel/scene-atlas/05-sceneManifest-resolution/sagas";
-import { HORUS_CONFIG } from "../horus-config/config";
-
-export async function fetchManifest(content: ParcelInfoResponse,
-    sceneId: string,
-    mappings?: any) {
-    return await fetchManifestForContent(content, sceneId, HORUS_CONFIG.CONTENT_SERVER_URL, mappings);
-}
