06c2b4ad2f09b712bc6f4396aa42cc8683ee69f4
Esteban Ordano <esteban@decentraland.org>
2020-01-04 19:42:52 -0300

    chore: move web3 to `c2-data`

--- webb/src/main.tsx
+++ webb/src/main.tsx
@@ -5,6 +5,11 @@ import * as Materials from '@babylonjs/materials'
 import { RootState } from 'dcl/kernel/core/types'
 import React from 'react'
 import { Avatar } from './avatar/Avatar'
+import { ScenePreview } from './scenePreview/ScenePreview'
+import { getCurrentWorldPosition } from 'dcl/kernel/scene-atlas/02-parcel-sight/selectors'
+import { setWorldPosition } from 'dcl/kernel/scene-atlas/01-user-position/actions'
+import { Position } from './controls/Position'
+import { Atlas } from './atlas/Atlas'
 
 console.log('Imported Loaders and Materials: ', Loaders, Materials)
 SceneLoader.OnPluginActivatedObservable.add((loader: any) => {
@@ -21,10 +26,18 @@ SceneLoader.OnPluginActivatedObservable.add((loader: any) => {
 })
 
 export function Main(props: { state: RootState; dispatch: any }) {
-  const { state } = props
+  const { state, dispatch } = props
+  const [currentScene, setCurrentScene] = React.useState()
+  const positionReport = getCurrentWorldPosition(state)
+  const setX = (x: number) => dispatch(setWorldPosition({ ...positionReport.position, x }))
+  const setY = (y: number) => dispatch(setWorldPosition({ ...positionReport.position, y }))
+  const setZ = (z: number) => dispatch(setWorldPosition({ ...positionReport.position, z }))
   return (
     <div>
       <Avatar {...state} />
+      {currentScene !== 'undefined' ? <ScenePreview {...state} currentScene={currentScene} /> : <div />}
+      <Position {...positionReport.position} {...{ setX, setY, setZ }}></Position>
+      <Atlas {...state} setCurrentScene={setCurrentScene}></Atlas>
     </div>
   )
 }
