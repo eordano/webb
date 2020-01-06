b36aa4fa4a88c59047a8026ef2155f3672086e3f
Esteban Ordano <esteban@decentraland.org>
2020-01-05 21:16:34 -0300

    fix navbar

--- memories/webapp/Navbar.tsx
+++ memories/webapp/Navbar.tsx
@@ -2,11 +2,21 @@ import React from 'react'
 export function Navbar(props: { id: string }) {
   return (
     <>
-      <a className='navbar' href='#prev'>&lt;&lt; first </a>
-      <a className='navbar' href='#prev'>&lt; back</a>
-      <a className='navbar' href='#prev'>🎲</a>
-      <a className='navbar' href='#prev'>next &gt;</a>
-      <a className='navbar' href='#prev'>last &gt; &gt;</a>
+      <a className='navbar' href='#prev'>
+        &lt;&lt; first{' '}
+      </a>
+      <a className='navbar' href='#prev'>
+        &lt; back
+      </a>
+      <a className='navbar' href='#prev'>
+        🎲
+      </a>
+      <a className='navbar' href='#prev'>
+        next &gt;
+      </a>
+      <a className='navbar' href='#prev'>
+        last &gt; &gt;
+      </a>
     </>
   )
 }
