4f20b082431edddc3ed56bf70ef8074add769c7a
Esteban Ordano <esteban@decentraland.org>
2020-03-04 02:27:42 -0300

    fix: minor updates

--- config/Communications.ts
+++ config/Communications.ts
@@ -1,12 +1,8 @@
 export const Communications = {
   debug: true,
-
   commRadius: 4,
-
   peerTtlMs: 60000,
-
   maxVisiblePeers: 25,
-
   ephemeralKeyTTL: 60 * 60 * 2,
 
   iceServers: [
@@ -23,9 +19,3 @@ export const Communications = {
     }
   ]
 }
-
-export const PUBLIC_KEY_SOURCE = 'https://auth.decentraland.today/api/v1/public_key'
-export const PINNED_PUBLIC_KEY = `-----BEGIN PUBLIC KEY-----
-MFkwEwYHKoZIzj0CAQYIKoZIzj0DAQcDQgAEo/cpceJdDp1Q/37nU1Ozd7BKCmai
-MtXhbxYR8Pk84qwGBBsrdbTLC1g/GfesANgxMEgrZShP2DxeaCB0rB8p7A==
------END PUBLIC KEY-----`
